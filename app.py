86c2db43cd67ccff484013a95f7e715e23df2e14
Iván Martínez <ivanmartinezdiaz9@gmail.com>
2026-04-25 14:47:28 -0300

    Actualización del proyecto

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def health():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=os.environ.get("FLASK_DEBUG") == "1")
